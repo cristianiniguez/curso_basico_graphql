969a7c5706ff971ef5a852d90c6e111b85c2e788
criniguez <felizpc1@gmail.com>
2020-12-16 16:35:19 -0400

    conceptos basicos: custom types

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 'use strict';
 
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const resolvers = require('./lib/resolvers');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // definiendo el esquema
-const schema = buildSchema(`
-  type Query {
-    hello: String
-    saludo: String
-  }
-`);
-
-// configurar los resolvers
-const resolvers = {
-  hello: () => 'Hola Mundo',
-  saludo: () => 'Hola a todos',
-};
+const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'));
 
 app.use(
   '/api',
